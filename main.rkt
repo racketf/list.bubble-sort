cac0a340b62c4055df39b1060079abe10e1be0ce
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:14:16 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,46 +1,46 @@
 #lang racket
 
-; (atmost1? lst): L -> B
+; (list.atmost1? lst): L -> B
 ; return #t if list has atmost 1 element(s).
-(define atmost1?
+(define list.atmost1?
   (lambda (lst)
     (or (null? lst) (null? (cdr lst)))))
 
-; (swap lst): L -> L
+; (list.swap lst): L -> L
 ; swaps the first two elements of list
-(define swap
+(define list.swap
   (lambda (lst)
-    (if (atmost1? lst)
+    (if (list.atmost1? lst)
         lst
         (cons (cadr lst) (cons (car lst) (cddr lst))))))
 
-; (swap-by lst f): L X F -> L
+; (list.swap-by lst f): L X F -> L
 ; swaps the first 2 elements of list using given function
-(define swap-by
+(define list.swap-by
   (lambda (lst f)
-    (if (or (atmost1? lst) (f (car lst) (cadr lst)))
+    (if (or (list.atmost1? lst) (f (car lst) (cadr lst)))
         lst
-        (swap lst))))
+        (list.swap lst))))
 
-; (bubble-once-by lst f): L X F -> L
+; (list.bubble-once-by lst f): L X F -> L
 ; runs a single pass of bubble sort on list
-(define bubble-once-by
+(define list.bubble-once-by
   (lambda (lst f)
-    (if (atmost1? lst)
+    (if (list.atmost1? lst)
         lst
-        (let ([lst (swap-by lst f)])
-          (cons (car lst) (bubble-once-by (cdr lst) f))))))
+        (let ([lst (list.swap-by lst f)])
+          (cons (car lst) (list.bubble-once-by (cdr lst) f))))))
 
-; (bubble-sort-by lst f): L X F -> L
+; (list.bubble-sort-by lst f): L X F -> L
 ; bubble sorts a list with given predicate f
-(define bubble-sort-by
+(define list.bubble-sort-by
   (lambda (lst f)
-    (if (atmost1? lst)
+    (if (list.atmost1? lst)
         lst
-        (bubble-once-by (cons (car lst) (bubble-sort-by (cdr lst) f)) f))))
+        (list.bubble-once-by (cons (car lst) (list.bubble-sort-by (cdr lst) f)) f))))
 
-; (bubble-sort lst): L -> L
+; (list.bubble-sort lst): L -> L
 ; bubble sorts a list in ascending order
-(define bubble-sort
+(define list.bubble-sort
   (lambda (lst)
-    (bubble-sort-by lst <=)))
+    (list.bubble-sort-by lst <=)))
